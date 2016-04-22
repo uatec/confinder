31d19b91a6331f8089f49a9d3bdd98cfd5dedd3a
Robert Stiff <uatecuk@gmail.com>
2016-04-22 20:28:57 +0100

    Simple map displayed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 var React = require('react');
+var GoogleMap = require('google-map-react');
 
 var Home = React.createClass({
   render: function() {
-    return <div>Hello World.</div>;
+    var center =  {lat: 59.938043, lng: 30.337157};
+    var zoom =  9;
+    return <div style={{width: '100%', height: '20em'}} > 
+            <GoogleMap
+                defaultCenter={center}
+                defaultZoom={zoom}>
+            </GoogleMap>
+        </div>;
   }
 });
 
