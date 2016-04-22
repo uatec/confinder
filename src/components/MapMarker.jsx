0f215d0583d1d917f10fa0d39f19cfd07b0688bb
Robert Stiff <uatecuk@gmail.com>
2016-04-22 21:54:25 +0100

    Simple hardcoded markers in the map.

---
+++ src/components/MapMarker.jsx
@@ -0,0 +1,9 @@
+var React = require('react');
+
+var MapMarker = React.createClass({
+   render: function() {
+       return <img src="images/pin.png" width="50" height="50" />;
+   }
+});
+
+module.exports = MapMarker;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,24 @@
 var React = require('react');
-var GoogleMap = require('google-map-react');
+var GoogleMap = require('google-map-react'),
+    MapMarker = require('../components/MapMarker.jsx');
+
 
 var Home = React.createClass({
   render: function() {
-    var center =  {lat: 59.938043, lng: 30.337157};
+      
+      var markers = [
+          <MapMarker lat={51.5} lng={0} />,
+          <MapMarker lat={51.5} lng={0.1} />,
+          <MapMarker lat={51.5} lng={0.2} />
+      ];
+      
+    var center =  {lat:51.4826, lng: 0.0077};
     var zoom =  9;
     return <div style={{width: '100%', height: '20em'}} > 
             <GoogleMap
                 defaultCenter={center}
                 defaultZoom={zoom}>
+                {markers}
             </GoogleMap>
         </div>;
   }
