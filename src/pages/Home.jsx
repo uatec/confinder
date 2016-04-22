9512414d0189a4ccbf72870dcf3427305ed8c0e0
Robert Stiff <uatecuk@gmail.com>
2016-04-22 21:58:10 +0100

    Have a basic header for a bit of branding.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,13 +14,20 @@ var Home = React.createClass({
       
     var center =  {lat:51.4826, lng: 0.0077};
     var zoom =  9;
-    return <div style={{width: '100%', height: '20em'}} > 
-            <GoogleMap
-                defaultCenter={center}
-                defaultZoom={zoom}>
-                {markers}
-            </GoogleMap>
-        </div>;
+    return  <div>
+                <center style={{backgroundColor:'#3F51B5', color: 'white'}}>
+                    <h1>Confinder</h1>
+                    <h3>Find out what's going on near you.</h3>
+                </center>
+            
+                <div style={{width: '100%', height: '20em'}} > 
+                    <GoogleMap
+                        defaultCenter={center}
+                        defaultZoom={zoom}>
+                        {markers}
+                    </GoogleMap>
+                </div>
+            </div>;
   }
 });
 
