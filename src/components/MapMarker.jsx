28b1c944ed203228946b070aae3b463c9b750b33
Robert Stiff <uatecuk@gmail.com>
2016-04-23 15:43:52 +0100

    Webpack Images and Tidy up generated assets.

--- src/components/MapMarker.jsx
+++ src/components/MapMarker.jsx
@@ -1,9 +1,11 @@
 var React = require('react');
 
+var pin = require('../images/pin.png');
+
 var MapMarker = React.createClass({
    render: function() {
         return <div>
-                 <img src="images/pin.png" style={{position: 'relative', top:-50, left: -25}} width="50" height="50" />
+                 <img src={pin} style={{position: 'relative', top:-50, left: -25}} width="50" height="50" />
                </div>;
    }
 });
--- webpack.config.js
+++ webpack.config.js
@@ -66,6 +66,7 @@ var loaders = [
                 test: /\.less$/,
                 loader: "style!css!less"
             },
+            { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192' }, // inline base64 URLs for <=8k images, direct URLs for the rest
             { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
             { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
             {
